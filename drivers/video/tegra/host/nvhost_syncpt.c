43b88c91c4a6cdadffd437e0ee50148463bd65bb
Sami Liedes <sliedes@nvidia.com>
2013-01-21 14:08:28 +0200

    video: tegra: host: Add newlines to sysfs nodes
    The files
    
      /sys/devices/platform/host1x/syncpt/*/{min,max}
    
    were not terminated with a newline. Fix this.
    
    Change-Id: I0680e6c0ec56635d046cbb731ff0eebb7d8743f7
    Signed-off-by: Sami Liedes <sliedes@nvidia.com>
    Reviewed-on: http://git-master/r/193024
    Reviewed-by: Juha Tukkinen <jtukkinen@nvidia.com>

--- drivers/video/tegra/host/nvhost_syncpt.c
+++ drivers/video/tegra/host/nvhost_syncpt.c
@@ -359,7 +359,7 @@ static ssize_t syncpt_min_show(struct kobject *kobj,
 	struct nvhost_syncpt_attr *syncpt_attr =
 		container_of(attr, struct nvhost_syncpt_attr, attr);
 
-	return snprintf(buf, PAGE_SIZE, "%u",
+	return snprintf(buf, PAGE_SIZE, "%u\n",
 			nvhost_syncpt_read(&syncpt_attr->host->syncpt,
 				syncpt_attr->id));
 }
@@ -370,7 +370,7 @@ static ssize_t syncpt_max_show(struct kobject *kobj,
 	struct nvhost_syncpt_attr *syncpt_attr =
 		container_of(attr, struct nvhost_syncpt_attr, attr);
 
-	return snprintf(buf, PAGE_SIZE, "%u",
+	return snprintf(buf, PAGE_SIZE, "%u\n",
 			nvhost_syncpt_read_max(&syncpt_attr->host->syncpt,
 				syncpt_attr->id));
 }
