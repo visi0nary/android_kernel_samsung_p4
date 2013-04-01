a701a42de9e22763971b241ea1682384dea49196
Alex Waterman <alexw@nvidia.com>
2013-04-01 13:24:59 -0700

    ARM: tegra: nvmap: Add free pages API for page pools
    This patch exposes the page pool available pages API to the rest of
    the kernel. Also, the API has been modified to not crash if the API
    is called before nvmap is initilized.
    
    Change-Id: I2352d0ad7132e3be4b658a4704515e27f40e2941
    Signed-off-by: Alex Waterman <alexw@nvidia.com>
    Signed-off-by: Krishna Reddy <vdumpa@nvidia.com>
    Reviewed-on: http://git-master/r/240265
    (cherry picked from commit ec83916d75f8c7e082589493c048ee884405b208)
    Reviewed-on: http://git-master/r/246828
    Reviewed-by: Automatic_Commit_Validation_User

--- arch/arm/mach-tegra/include/mach/nvmap.h
+++ arch/arm/mach-tegra/include/mach/nvmap.h
@@ -138,6 +138,15 @@ int nvmap_patch_word(struct nvmap_client *client,
 		     struct nvmap_handle *patch,
 		     u32 patch_offset, u32 patch_value);
 
+#ifdef CONFIG_NVMAP_PAGE_POOLS
+ulong nvmap_page_pool_get_unused_pages(void);
+#else
+static inline nvmap_page_pool_get_unused_pages(void)
+{
+	return 0;
+}
+#endif
+
 struct nvmap_platform_carveout {
 	const char *name;
 	unsigned int usage_mask;
