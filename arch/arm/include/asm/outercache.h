e20a6b282ac91888894aefda7a3f3b839c21cf9a
Russell King <rmk+kernel@arm.linux.org.uk>
2014-03-15 16:47:44 +0000

    ARM: l2c: remove outer_inv_all() method
    No one ever calls this function anywhere in the kernel, so let's
    completely remove it from the outer cache API and turn it into an
    internal-only thing.
    
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/include/asm/outercache.h
+++ arch/arm/include/asm/outercache.h
@@ -29,7 +29,6 @@ struct outer_cache_fns {
 	void (*flush_range)(unsigned long, unsigned long);
 	void (*flush_all)(void);
 	void (*clean_all)(void);
-	void (*inv_all)(void);
 	void (*disable)(void);
 #ifdef CONFIG_OUTER_CACHE_SYNC
 	void (*sync)(void);
@@ -70,12 +69,6 @@ static inline void outer_clean_all(void)
 		outer_cache.clean_all();
 }
 
-static inline void outer_inv_all(void)
-{
-	if (outer_cache.inv_all)
-		outer_cache.inv_all();
-}
-
 static inline void outer_disable(void)
 {
 	if (outer_cache.disable)
@@ -98,7 +91,6 @@ static inline void outer_flush_range(phys_addr_t start, phys_addr_t end)
 { }
 static inline void outer_flush_all(void) { }
 static inline void outer_clean_all(void) { }
-static inline void outer_inv_all(void) { }
 static inline void outer_disable(void) { }
 
 #endif
