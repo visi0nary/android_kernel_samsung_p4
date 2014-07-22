ed253aac3991efd665dfd477bf41100db673e600
RyanG <decatf@gmail.com>
2014-07-22 15:13:42 -0400

    video: tegra: host: Restrict register access
    Register access (read/write) to modules MPE, ISP and VI lack sanity
    check for the register number. Add checks to ensure only aperture is
    accessed.
    
    Bug 992938
    
    
    aosp kernel/tegra android-tegra3-grouper-3.1-kitkat-mr2
    c42c9fac241aaa1497d5b9eb913cc08cccaf8020

--- drivers/video/tegra/host/bus_client.c
+++ drivers/video/tegra/host/bus_client.c
@@ -48,10 +48,15 @@
 #include "nvhost_job.h"
 #include "nvhost_hwctx.h"
 
-void nvhost_read_module_regs(struct nvhost_device *ndev,
+int nvhost_read_module_regs(struct nvhost_device *ndev,
 			u32 offset, int count, u32 *values)
 {
 	void __iomem *p = ndev->aperture + offset;
+
+	/* verify offset */
+	struct resource *r = nvhost_get_resource(ndev, IORESOURCE_MEM, 0);
+	if (offset + 4 * count >= resource_size(r))
+		return -EPERM;
 
 	nvhost_module_busy(ndev);
 	while (count--) {
@@ -60,12 +65,19 @@ void nvhost_read_module_regs(struct nvhost_device *ndev,
 	}
 	rmb();
 	nvhost_module_idle(ndev);
+
+	return 0;
 }
 
-void nvhost_write_module_regs(struct nvhost_device *ndev,
+int nvhost_write_module_regs(struct nvhost_device *ndev,
 			u32 offset, int count, const u32 *values)
 {
 	void __iomem *p = ndev->aperture + offset;
+
+	/* verify offset */
+	struct resource *r = nvhost_get_resource(ndev, IORESOURCE_MEM, 0);
+	if (offset + 4 * count >= resource_size(r))
+		return -EPERM;
 
 	nvhost_module_busy(ndev);
 	while (count--) {
@@ -74,6 +86,8 @@ void nvhost_write_module_regs(struct nvhost_device *ndev,
 	}
 	wmb();
 	nvhost_module_idle(ndev);
+
+	return 0;
 }
 
 struct nvhost_channel_userctx {
