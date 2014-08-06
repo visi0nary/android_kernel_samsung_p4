555d6cf387fde41775e6e85ecb656f2fd949b523
RyanG <decatf@gmail.com>
2014-08-06 18:10:03 -0400

    video: tegra: dc: use 64-bit arithmetic
    Use u64 to avoid overflowing 32-bit values.

--- drivers/video/tegra/dc/bandwidth.c
+++ drivers/video/tegra/dc/bandwidth.c
@@ -16,6 +16,7 @@
 
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/math64.h>
 
 #include <mach/clk.h>
 #include <mach/dc.h>
@@ -130,7 +131,7 @@ static unsigned long tegra_dc_find_max_bandwidth(struct tegra_dc_win *wins[],
 static unsigned long tegra_dc_calc_win_bandwidth(struct tegra_dc *dc,
 	struct tegra_dc_win *w)
 {
-	unsigned long ret;
+	u64 ret;
 	int tiled_windows_bw_multiplier;
 	unsigned long bpp;
 	unsigned in_w;
@@ -155,19 +156,19 @@ static unsigned long tegra_dc_calc_win_bandwidth(struct tegra_dc *dc,
 	 * is of the luma plane's size only. */
 	bpp = tegra_dc_is_yuv_planar(w->fmt) ?
 		2 * tegra_dc_fmt_bpp(w->fmt) : tegra_dc_fmt_bpp(w->fmt);
-	ret = dc->mode.pclk / 1000UL * bpp / 8 *
+	ret = (dc->mode.pclk / 1000UL * bpp / 8);
 #if defined(CONFIG_ARCH_TEGRA_2x_SOC) || defined(CONFIG_ARCH_TEGRA_3x_SOC)
-		(win_use_v_filter(dc, w) ? 2 : 1) *
+	ret *= (win_use_v_filter(dc, w) ? 2 : 1);
 #endif
-			in_w / w->out_w * (WIN_IS_TILED(w) ?
-			tiled_windows_bw_multiplier : 1);
+	ret *=	div_u64(in_w, w->out_w * (WIN_IS_TILED(w) ?
+			tiled_windows_bw_multiplier : 1));
 
 #ifdef CONFIG_ARCH_TEGRA_2x_SOC
 	/*
 	 * Assuming 60% efficiency: i.e. if we calculate we need 70MBps, we
 	 * will request 117MBps from EMC.
 	 */
-	ret = ret + (17 * ret / 25);
+	ret = ret + (17 * div_u64(ret, 25));
 #else
 /*
 	 * Assuming ~35% efficiency: i.e. if we calculate we need 70MBps, we
