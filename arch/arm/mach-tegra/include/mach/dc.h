7fd1613d928f6c0881924ef63cf88c25ce4d835f
RyanG <decatf@gmail.com>
2014-08-06 20:01:00 -0400

    video: tegra: dc: Add CMU support

--- arch/arm/mach-tegra/include/mach/dc.h
+++ arch/arm/mach-tegra/include/mach/dc.h
@@ -379,6 +379,8 @@ struct tegra_dc_out {
 #define TEGRA_DC_OUT_ONE_SHOT_MODE		(1 << 3)
 #define TEGRA_DC_OUT_N_SHOT_MODE		(1 << 4)
 #define TEGRA_DC_OUT_ONE_SHOT_LP_MODE		(1 << 5)
+#define TEGRA_DC_OUT_CMU_DISABLE		(0 << 5)
+#define TEGRA_DC_OUT_CMU_ENABLE			(1 << 5)
 
 #define TEGRA_DC_ALIGN_MSB		0
 #define TEGRA_DC_ALIGN_LSB		1
@@ -405,6 +407,24 @@ struct tegra_dc_lut {
 	u8 r[256];
 	u8 g[256];
 	u8 b[256];
+};
+
+struct tegra_dc_cmu_csc {
+	u16 krr;
+	u16 kgr;
+	u16 kbr;
+	u16 krg;
+	u16 kgg;
+	u16 kbg;
+	u16 krb;
+	u16 kgb;
+	u16 kbb;
+};
+
+struct tegra_dc_cmu {
+	u16 lut1[256];
+	struct tegra_dc_cmu_csc csc;
+	u8 lut2[960];
 };
 
 struct tegra_dc_win {
