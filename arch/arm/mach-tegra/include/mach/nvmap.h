d6e694579a28c37fb2e6b7f48a91c2b50820a9d8
RyanG <decatf@gmail.com>
2014-08-13 19:29:07 -0400

    video: tegra: nvmap: Add support for zeroed pages
    Add support to alloc zeroed pages for user space alloc requests. Also
    define a config option to force userspace allocation requests to be
    zeroed.

--- arch/arm/mach-tegra/include/mach/nvmap.h
+++ arch/arm/mach-tegra/include/mach/nvmap.h
@@ -50,7 +50,7 @@
 #define NVMAP_HANDLE_CACHE_FLAG      (0x3ul << 0)
 
 #define NVMAP_HANDLE_SECURE          (0x1ul << 2)
-
+#define NVMAP_HANDLE_ZEROED_PAGES    (0x1ul << 3)
 
 #if defined(__KERNEL__)
 
