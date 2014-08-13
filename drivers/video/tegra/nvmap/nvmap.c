2d7585b5d1ff9b97ac7e56b5f9e933f8407f2836
RyanG <decatf@gmail.com>
2014-08-13 19:25:38 -0400

    video: tegra: nvmap: fix usermode input validation
    nvmap_pin_ids accepts a list of handles to pin, which are passed
    directly from usermode when called from nvmap_ioctl_pinop.
    
    However, if invalid handles are passed in, the function might
    still access them as valid handles, and cause a crash.

--- drivers/video/tegra/nvmap/nvmap.c
+++ drivers/video/tegra/nvmap/nvmap.c
@@ -275,7 +275,7 @@ int nvmap_pin_ids(struct nvmap_client *client,
 	 * if the caller crashes after pinning a global handle, the handle
 	 * will be permanently leaked. */
 	nvmap_ref_lock(client);
-	for (i = 0; i < nr && !ret; i++) {
+	for (i = 0; i < nr; i++) {
 		ref = _nvmap_validate_id_locked(client, ids[i]);
 		if (ref) {
 			atomic_inc(&ref->pin);
@@ -284,18 +284,18 @@ int nvmap_pin_ids(struct nvmap_client *client,
 			struct nvmap_handle *verify;
 			nvmap_ref_unlock(client);
 			verify = nvmap_validate_get(client, ids[i]);
-			if (verify)
+			if (verify) {
 				nvmap_warn(client, "%s pinning unreferenced "
 					   "handle %p\n",
 					   current->group_leader->comm, h[i]);
-			else
+			} else {
+				h[i] = NULL;
 				ret = -EPERM;
+			}
 			nvmap_ref_lock(client);
 		}
 	}
 	nvmap_ref_unlock(client);
-
-	nr = i;
 
 	if (ret)
 		goto out;
@@ -321,6 +321,9 @@ out:
 	if (ret) {
 		nvmap_ref_lock(client);
 		for (i = 0; i < nr; i++) {
+			if(!ids[i])
+				continue;
+
 			ref = _nvmap_validate_id_locked(client, ids[i]);
 			if (!ref) {
 				nvmap_warn(client, "%s freed handle %p "
@@ -334,7 +337,8 @@ out:
 		nvmap_ref_unlock(client);
 
 		for (i = 0; i < nr; i++)
-			nvmap_handle_put(h[i]);
+			if(h[i])
+				nvmap_handle_put(h[i]);
 	}
 
 	return ret;
