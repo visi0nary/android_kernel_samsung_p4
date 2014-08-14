4e30e37f53e17931ac940f73346d0fb5ad360ffb
RyanG <decatf@gmail.com>
2014-08-14 17:54:47 -0400

    video: tegra: dc: change enable and hotplug_init arguments
    pass device as argument for enable and hotplug_init so that it
    can be used in board files to get the regulator using device name.
    
    Bug 1154495

--- drivers/video/tegra/dc/dc.c
+++ drivers/video/tegra/dc/dc.c
@@ -1635,7 +1635,7 @@ static bool _tegra_dc_controller_enable(struct tegra_dc *dc)
 	int failed_init = 0;
 
 	if (dc->out->enable)
-		dc->out->enable();
+		dc->out->enable(&dc->ndev->dev);
 
 	tegra_dc_setup_clk(dc, dc->clk);
 #ifdef CONFIG_MACH_SAMSUNG_VARIATION_TEGRA
@@ -1720,7 +1720,7 @@ static bool _tegra_dc_controller_reset_enable(struct tegra_dc *dc)
 	bool ret = true;
 
 	if (dc->out->enable)
-		dc->out->enable();
+		dc->out->enable(&dc->ndev->dev);
 
 	tegra_dc_setup_clk(dc, dc->clk);
 	tegra_dc_clk_enable(dc);
@@ -2294,7 +2294,7 @@ static int tegra_dc_probe(struct nvhost_device *ndev,
 	}
 
 	if (dc->out && dc->out->hotplug_init)
-		dc->out->hotplug_init();
+		dc->out->hotplug_init(&ndev->dev);
 
 	if (dc->out_ops && dc->out_ops->detect)
 		dc->out_ops->detect(dc);
@@ -2410,7 +2410,7 @@ static int tegra_dc_resume(struct nvhost_device *ndev)
 	}
 
 	if (dc->out && dc->out->hotplug_init)
-		dc->out->hotplug_init();
+		dc->out->hotplug_init(&ndev->dev);
 
 	if (dc->out_ops && dc->out_ops->resume)
 		dc->out_ops->resume(dc);
