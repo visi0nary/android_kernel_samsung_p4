9532758250bb5f2fecfeed0ecb15c18a07733466
RyanG <decatf@gmail.com>
2014-08-14 17:41:14 -0400

    tegra:dc: Init  default mode on HDMI
    Set default videomode on DC before enabling HDMI.When running
    framebuffer console on HDMI, a default mode must be set before
    calling init, else tegra_dc_init will fail due to 0 pclk.
    
    Bug 1046489

--- drivers/video/tegra/fb.c
+++ drivers/video/tegra/fb.c
@@ -29,6 +29,7 @@
 #include <linux/slab.h>
 #include <linux/file.h>
 #include <linux/workqueue.h>
+#include <linux/console.h>
 
 #include <linux/atomic.h>
 
@@ -524,7 +525,13 @@ void tegra_fb_update_monspecs(struct tegra_fb_info *fb_info,
 	}
 
 	event.info = fb_info->info;
+#ifdef CONFIG_FRAMEBUFFER_CONSOLE
+	console_lock();
 	fb_notifier_call_chain(FB_EVENT_NEW_MODELIST, &event);
+	console_unlock();
+#else
+	fb_notifier_call_chain(FB_EVENT_NEW_MODELIST, &event);
+#endif
 	mutex_unlock(&fb_info->info->lock);
 }
 
