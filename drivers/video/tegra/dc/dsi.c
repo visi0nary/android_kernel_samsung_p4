ca96f0f6c4f9bc35873fe478b7d23f179d162280
RyanG <decatf@gmail.com>
2014-08-18 17:30:00 -0400

    Revert "video: tegra: dsi: Disable syncpt use"
    This reverts commit ecb137c115c600972a3c42ab0d6b2be905e36d98.
    
    It seems like it was disabled for t114.

--- drivers/video/tegra/dc/dsi.c
+++ drivers/video/tegra/dc/dsi.c
@@ -46,7 +46,7 @@
 #define APB_MISC_GP_MIPI_PAD_CTRL_0	(TEGRA_APB_MISC_BASE + 0x820)
 #define DSIB_MODE_ENABLE		0x2
 
-#define DSI_USE_SYNC_POINTS		0
+#define DSI_USE_SYNC_POINTS		1
 #define S_TO_MS(x)			(1000 * (x))
 #define MS_TO_US(x)			(1000 * (x))
 
@@ -409,8 +409,7 @@ static inline void tegra_dsi_clk_disable(struct tegra_dc_dsi_data *dsi)
 		clk_disable_unprepare(dsi->dsi_clk);
 }
 
-static void __maybe_unused tegra_dsi_syncpt_reset(
-				struct tegra_dc_dsi_data *dsi)
+static void tegra_dsi_syncpt_reset(struct tegra_dc_dsi_data *dsi)
 {
 	tegra_dsi_writel(dsi, 0x1, DSI_INCR_SYNCPT_CNTRL);
 	/* stabilization delay */
@@ -420,7 +419,7 @@ static void __maybe_unused tegra_dsi_syncpt_reset(
 	udelay(300);
 }
 
-static int __maybe_unused tegra_dsi_syncpt(struct tegra_dc_dsi_data *dsi)
+static int tegra_dsi_syncpt(struct tegra_dc_dsi_data *dsi)
 {
 	u32 val;
 	int ret = 0;
@@ -3012,14 +3011,14 @@ static int tegra_dsi_enter_ulpm(struct tegra_dc_dsi_data *dsi)
 	if (ret < 0) {
 		dev_err(&dsi->dc->ndev->dev,
 			"DSI syncpt for ulpm enter failed\n");
-		return ret;
+		goto fail;
 	}
 #else
 	/* TODO: Find exact delay required */
 	mdelay(10);
 #endif
 	dsi->ulpm = true;
-
+fail:
 	return ret;
 }
 
@@ -3041,7 +3040,7 @@ static int tegra_dsi_exit_ulpm(struct tegra_dc_dsi_data *dsi)
 	if (ret < 0) {
 		dev_err(&dsi->dc->ndev->dev,
 			"DSI syncpt for ulpm exit failed\n");
-		return ret;
+		goto fail;
 	}
 #else
 	/* TODO: Find exact delay required */
@@ -3053,8 +3052,9 @@ static int tegra_dsi_exit_ulpm(struct tegra_dc_dsi_data *dsi)
 	val &= ~DSI_HOST_DSI_CONTROL_ULTRA_LOW_POWER(0x3);
 	val |= DSI_HOST_DSI_CONTROL_ULTRA_LOW_POWER(NORMAL);
 	tegra_dsi_writel(dsi, val, DSI_HOST_DSI_CONTROL);
-
+fail:
 	return ret;
+
 }
 
 static void tegra_dsi_send_dc_frames(struct tegra_dc *dc,
