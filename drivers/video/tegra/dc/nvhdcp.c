6eae4e08bb7dba2bd400cbec3bb0037697f98bc6
RyanG <decatf@gmail.com>
2014-08-19 23:08:14 -0400

    video: tegra: dc: power host1x during HDCP link
    Add tegra_dc_io_start/end to take host1x references while negotiating HDCP.
    Without these the module can be turned off and some state won't be
    programmed properly.
    
    Bug 1176386
    
    Change-Id: I1893b017f09a406965b478b0a9de1730b16ca0ac
    Signed-off-by: Jon Mayo <jmayo@nvidia.com>
    Reviewed-on: http://git-master/r/164809
    Reviewed-by: Simone Willett <swillett@nvidia.com>
    Tested-by: Simone Willett <swillett@nvidia.com>

--- drivers/video/tegra/dc/nvhdcp.c
+++ drivers/video/tegra/dc/nvhdcp.c
@@ -954,12 +954,15 @@ static void nvhdcp_downstream_worker(struct work_struct *work)
 	struct tegra_nvhdcp *nvhdcp =
 		container_of(to_delayed_work(work), struct tegra_nvhdcp, work);
 	struct tegra_dc_hdmi_data *hdmi = nvhdcp->hdmi;
+	struct tegra_dc *dc = tegra_dc_hdmi_get_dc(hdmi);
 	int e;
 	u8 b_caps;
 	u32 tmp;
 	u32 res;
 	bool hdcp_state_changed = false;
+
 	nvhdcp_vdbg("%s():started thread %s\n", __func__, nvhdcp->name);
+	tegra_dc_io_start(dc);
 
 	mutex_lock(&nvhdcp->lock);
 	if (nvhdcp->state == STATE_OFF) {
@@ -1138,8 +1141,10 @@ static void nvhdcp_downstream_worker(struct work_struct *work)
 			goto failure;
 		}
 		mutex_unlock(&nvhdcp->lock);
+		tegra_dc_io_end(dc);
 		wait_event_interruptible_timeout(wq_worker,
 			!nvhdcp_is_plugged(nvhdcp), msecs_to_jiffies(1500));
+		tegra_dc_io_start(dc);
 		mutex_lock(&nvhdcp->lock);
 
 	}
@@ -1166,11 +1171,13 @@ lost_hdmi:
 
 err:
 	mutex_unlock(&nvhdcp->lock);
+	tegra_dc_io_end(dc);
 	return;
 disable:
 	nvhdcp->state = STATE_OFF;
 	nvhdcp_set_plugged(nvhdcp, false);
 	mutex_unlock(&nvhdcp->lock);
+	tegra_dc_io_end(dc);
 	return;
 }
 
