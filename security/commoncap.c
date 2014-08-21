649706ffbac3f4bdaa9162a216a961e93cf86b66
RyanG <decatf@gmail.com>
2014-08-20 23:34:10 -0400

    fix: userns: Use cred->user_ns instead of cred->user->user_ns

--- security/commoncap.c
+++ security/commoncap.c
@@ -232,7 +232,7 @@ static inline int cap_inh_is_capped(void)
 	/* they are so limited unless the current task has the CAP_SETPCAP
 	 * capability
 	 */
-	if (cap_capable(current_cred(), current_cred()->user->user_ns,
+	if (cap_capable(current_cred(), current_cred()->user_ns,
 			CAP_SETPCAP, SECURITY_CAP_AUDIT) == 0)
 		return 0;
 	return 1;
