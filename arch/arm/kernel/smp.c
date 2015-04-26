31cd532f300a25d49c1bdb1cc4c47e9c6a81eef2
RyanG <decatf@gmail.com>
2015-04-26 19:55:05 -0400

    Revert "ARM: 7587/1: implement optimized percpu variable access"
    This reverts commit 27c248599be8657e8b2c76dbda053f8282bcacec.

--- arch/arm/kernel/smp.c
+++ arch/arm/kernel/smp.c
@@ -357,8 +357,6 @@ void __init smp_prepare_boot_cpu(void)
 	unsigned int cpu = smp_processor_id();
 
 	per_cpu(cpu_data, cpu).idle = current;
-
-	set_my_cpu_offset(per_cpu_offset(smp_processor_id()));
 }
 
 void __init smp_prepare_cpus(unsigned int max_cpus)
