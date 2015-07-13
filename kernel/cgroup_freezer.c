a51b6a227034bf9c244b281884ef6e73d85fb2bc
RyanG <decatf@gmail.com>
2015-07-13 14:09:09 -0400

    cgroup_freezer: Fix build

--- kernel/cgroup_freezer.c
+++ kernel/cgroup_freezer.c
@@ -176,7 +176,7 @@ static int freezer_can_attach(struct cgroup_subsys *ss,
 	 * Anything frozen can't move or be moved to/from.
 	 */
 	cgroup_taskset_for_each(task, new_cgroup, tset)
-		if (cgroup_freezing(task))
+		if (__cgroup_freezing_or_frozen(task))
 			return -EBUSY;
 
 	freezer = cgroup_freezer(new_cgroup);
