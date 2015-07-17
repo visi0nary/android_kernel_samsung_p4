f1526497991a308971287482f0da733e14281669
RyanG <decatf@gmail.com>
2015-07-17 12:19:56 -0400

    net: wireless: bcmdhd: Reduce HW4 logging level

--- drivers/net/wireless/bcmdhd/wl_cfgp2p.c
+++ drivers/net/wireless/bcmdhd/wl_cfgp2p.c
@@ -1168,7 +1168,7 @@ wl_cfgp2p_set_management_ie(struct wl_priv *wl, struct net_device *ndev, s32 bss
 				parsed_ie_buf_len += vndrie_info->ie_len;
 			}
 		}
-		CFGP2P_ERR(("### CSP 675188 debug_log %s(%d)\n", __func__, __LINE__));
+		CFGP2P_DBG(("### CSP 675188 debug_log %s(%d)\n", __func__, __LINE__));
 		if (mgmt_ie_buf != NULL || mgmt_ie_len != NULL) {
 			if (parsed_ie_buf_len && (parsed_ie_buf_len == *mgmt_ie_len) &&
 			     (memcmp(mgmt_ie_buf, curr_ie_buf, parsed_ie_buf_len) == 0)) {
