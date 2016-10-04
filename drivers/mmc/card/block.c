98047d442700d9bd7bee3b2ff1d96a7d8364b72d
ryang <decatf@gmail.com>
2016-10-04 13:50:06 -0400

    Revert "mmc: card: Issue BKOPS when queue is empty"

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -1122,9 +1122,6 @@ static int mmc_blk_issue_rw_rq(struct mmc_queue *mq, struct request *rqc)
 		}
 	} while (ret);
 
-	if (brq->cmd.resp[0] & R1_URGENT_BKOPS)
-		mmc_card_set_need_bkops(card);
-
 	return 1;
 
  cmd_err:
@@ -1207,10 +1204,6 @@ static int mmc_blk_issue_rq(struct mmc_queue *mq, struct request *req)
 			mmc_blk_issue_rw_rq(mq, NULL);
 		ret = mmc_blk_issue_flush(mq, req);
 	} else {
-		/* Abort any current bk ops of eMMC card by issuing HPI */
-		if (mmc_card_mmc(mq->card) && mmc_card_doing_bkops(mq->card))
-			mmc_interrupt_hpi(mq->card);
-
 		ret = mmc_blk_issue_rw_rq(mq, req);
 	}
 
